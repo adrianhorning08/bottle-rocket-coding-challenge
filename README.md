6e12d70fea02d225d8057222300ccdd7a6679ff7
Adrian Horning <m.adrian.horning@gmail.com>
2018-03-31 15:02:18 -0700

    Holy shit the gradient finally works

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Instructions:
 
 Download and run "npm install" to install dependencies and then "npm run start"
 
+* I downloaded the font for free online I think they may have some built in padding, so it may be a little off
 
 List View and Detail View need to be on the same page.
 In react, they would both exist at root? Or would they have different urls?
--- frontend/scripts/components/restaurantIndexItem.jsx
+++ frontend/scripts/components/restaurantIndexItem.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const RestaurantIndexItem = props => {
   return (
     <section className="restaurant-index-item"
-      style={{ backgroundImage: `url(${props.restaurant.backgroundImageURL})`}}>
+      style={
+        {
+          backgroundImage: `url("http://res.cloudinary.com/dqw6az5ot/image/upload/v1522533459/cellGradientBackground_2x.png"),
+          url(${props.restaurant.backgroundImageURL})`
+        }
+      }>
       <div className="restaurant-index-item-text">
         <h2>{props.restaurant.name}</h2>
         <h3>{props.restaurant.category}</h3>
